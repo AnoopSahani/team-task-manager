6dc7c30c4a0401e8b1c9f2ac0b82158be00de51a
Anoop Sahani <anoopsahani63@gmail.com>
2026-05-16 13:07:11 +0530

    change assets dir to avoid railway 403

--- team-task-manager/frontend/vite.config.js
+++ team-task-manager/frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     port: 5173,
     proxy: {
@@ -14,5 +15,6 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist',
+    assetsDir: 'static-assets',
   }
 })
